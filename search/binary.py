d6101f5de441094bd407b30b67af4d68cccbab1d
thiagoandre <thiagoandrecardoso@icloud.com>
2023-07-19 17:47:06 -0300

    initial commit

---
+++ search/binary.py
@@ -0,0 +1,20 @@
+def binary_search(lst, item):
+    low = 0
+    high = len(lst) - 1
+
+    while low <= high:
+        middle = (high + low) // 2
+        kick = lst[middle]
+        if kick == item:
+            return middle
+        if kick > item:
+            high = middle - 1
+        else:
+            low = middle + 1
+    return None
+
+
+number_list = [1, 3, 5, 7, 9]
+
+print(binary_search(number_list, 3))
+print(binary_search(number_list, -1))
