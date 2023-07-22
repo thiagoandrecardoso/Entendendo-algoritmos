103bfb92b69861579824ba6f2e13f522a8843dc0
thiagoandre <thiagoandrecardoso@icloud.com>
2023-07-22 00:48:21 -0300

    Adjustment function name.

--- sorting/selection_sorting.py
+++ sorting/selection_sorting.py
@@ -1,4 +1,5 @@
-def search_minor(arr):
+# search for the index of the smallest value
+def find_min_index(arr):
     minor = arr[0]
     minor_index = 0
     for i in range(1, len(arr)):
@@ -11,7 +12,7 @@ def search_minor(arr):
 def sorting_by_selection(arr):
     new_arr = []
     for i in range(len(arr)):
-        minor = search_minor(arr)
+        minor = find_min_index(arr)
         new_arr.append(arr.pop(minor))
     return new_arr
 
