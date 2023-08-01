550e6f1e46e519bd5de155859538352e4abb8b76
thiagoandre <thiagoandrecardoso@icloud.com>
2023-07-31 21:27:32 -0300

    Quicksort by ChatGPT

--- quicksort/quicksort.py
+++ quicksort/quicksort.py
@@ -1,3 +1,4 @@
+# Book
 def quicksort(array):
     if len(array) < 2:
         return array
@@ -9,3 +10,26 @@ def quicksort(array):
 
 
 print(quicksort([10, 5, 2, 3]))
+
+
+# ChatGPT
+def quicksort(array):
+    if len(array) < 2:
+        return array
+
+    # Choice of pivot as the median of three (first, last and middle element).
+    mid = len(array) // 2
+    pivot = sorted([array[0], array[mid], array[-1]])[1]
+
+    # List partitioning
+    left = [x for x in array if x < pivot]
+    equal = [x for x in array if x == pivot]
+    right = [x for x in array if x > pivot]
+
+    return quicksort(left) + equal + quicksort(right)
+
+
+# Example of use:
+my_array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+sorted_array = quicksort(my_array)
+print(sorted_array)
