623bad9867b0a3712a1dd6dc1fb5e2ca5ba46316
thiagoandre <thiagoandrecardoso@icloud.com>
2023-08-09 10:48:08 -0300

    Exercises hash Study on hash table finalized.

---
+++ hash/exercises.py
@@ -0,0 +1,9 @@
+# 5.5
+telephone_list = {"Esther": 988548890, "Ben": 988990077, "Bob": 988776655, "Dan": 988554466}
+
+# 5.6
+battery_power = {"A": 3.5, "AA": 2.5, "AAA": 1.5, "AAAA": 0.6}
+
+# 5.7
+books_authors = {"Maus": "Art Spiegelman", "Fun Home": "Alison Bechdel", "Watchman": "Alan Moore"}
+
